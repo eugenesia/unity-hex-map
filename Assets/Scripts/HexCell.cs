219807b16b348789a00facbe6cfa5e3710b5c399
eugenesia <eugene@eugenesia.co.uk>
2018-06-30 15:04:21 +0100

    Generate mesh

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -5,4 +5,16 @@ public class HexCell : MonoBehaviour {
 	//public HexCoordinates coordinates;
 
 	public Color color;
+
+	private void Awake()
+	{
+        Mesh m = new Mesh();
+        GetComponent<MeshFilter>().mesh = m;
+        m.vertices = [
+            new Vector3(1, 0, 1),
+            new Vector3(1, 0, -1),
+            new Vector3(-1, 0, -1),
+            new Vector3(1, 0, 1)
+        ];
+	}
 }
