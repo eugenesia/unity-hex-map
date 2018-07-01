8e078ab15ca3070f5e9f73ada89a1f47f6733ef8
eugenesia <eugene@eugenesia.co.uk>
2018-07-01 21:52:08 +0100

    Add HexCoordinates struct

---
+++ Assets/Scripts/HexCoordinates.cs
@@ -0,0 +1,11 @@
+﻿/**
+ * Convert coordinate systems.
+ */
+using UnityEngine;
+
+// This is so that Unity will save settings we put in Inspector,
+// and reload even if we change the script.
+[System.Serializable]
+public struct HexCoordinates {
+    
+}
