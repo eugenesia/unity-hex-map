fed60cb8a93d05a2e44a462fdea02ddac092530f
eugenesia <eugene@eugenesia.co.uk>
2018-07-01 12:59:44 +0100

    Don't draw mesh on Hex Cell (draw everything in Grid)

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -5,18 +5,4 @@ public class HexCell : MonoBehaviour {
 	//public HexCoordinates coordinates;
 
 	public Color color;
-    public Vector3[] vertices;
-    public Vector2[] uv;
-    public int[] triangles;
-
-	private void Awake()
-	{
-        // Initialise new mesh
-        // https://docs.unity3d.com/ScriptReference/Mesh.html
-        Mesh m = new Mesh();
-        GetComponent<MeshFilter>().mesh = m;
-        m.vertices = vertices;
-        m.uv = uv;
-        m.triangles = triangles;
-	}
 }
