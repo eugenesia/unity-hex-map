a88dc0515a218734a3b0ce648a15dbc6fc8915c6
eugenesia <eugene@eugenesia.co.uk>
2018-07-02 01:17:17 +0100

    Calculate and show Y coord on label

--- Assets/Scripts/HexCoordinates.cs
+++ Assets/Scripts/HexCoordinates.cs
@@ -12,6 +12,14 @@ public struct HexCoordinates {
     public int X { get; private set; }
     public int Z { get; private set; }
 
+    // Y coord is redundant as X + Y + Z = 0
+    // Don't store it but calculate it on demand.
+    public int Y {
+        get {
+            return -X - Z;
+        }
+    }
+
     public HexCoordinates (int x, int z) {
         X = x;
         Z = z;
@@ -24,10 +32,11 @@ public struct HexCoordinates {
     }
 
 	public override string ToString() {
-        return "(" + X.ToString() + ", " + Z.ToString() + ")";
+        return "(" +
+            X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
 	}
 
     public string ToStringOnSeparateLines() {
-        return X.ToString() + "\n" + Z.ToString();
+        return X.ToString() + "\n" + Y.ToString() + "\n" + Z.ToString();
     }
 }
