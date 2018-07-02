5d45cb53e3fd1f8474e0d1539b8b46a304b452e5
eugenesia <eugene@eugenesia.co.uk>
2018-07-02 02:04:02 +0100

    Use PropertyDrawer to display hex coords in Inspector

---
+++ Assets/Scripts/HexCoordinatesDrawer.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * Customise the look of hex coords in the Inspector during game mode
+ * by using a Property Drawer.
+ */
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(HexCoordinates))]
+public class HexCoordinatesDrawer : PropertyDrawer {
+	public override void OnGUI(
+        Rect position, SerializedProperty property, GUIContent label) {
+
+        // Convert to hex coords object.
+        HexCoordinates coordinates = new HexCoordinates(
+            property.FindPropertyRelative("x").intValue,
+            property.FindPropertyRelative("z").intValue
+        );
+
+        // Return an adjusted rectangle matching the space to right of label.
+        position = EditorGUI.PrefixLabel(position, label);
+
+        GUI.Label(position, coordinates.ToString());
+	}
+}
